9ffb7e8897fd630e19e999ebab56f1cb541b245f
antonio <antoniobiskupec1@gmail.com>
2025-06-15 19:01:02 +0200

    first commit, added main logic for adding items to list

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Logo from "./components/Logo.jsx";
+import Form from "./components/Form.jsx";
+import ItemList from "./components/ItemList.jsx";
+import Stats from "./components/Stats.jsx";
+
+function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <ItemList />
+      <Stats />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ItemList.jsx
@@ -0,0 +1,18 @@
+import Item from "./Item";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
+export default function ItemList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
