436a3c32825b67ab742e74a4b8bcad748dd0cab4
antonio <antoniobiskupec1@gmail.com>
2025-06-23 19:30:38 +0200

    aded select options that sort items by input time, description or packed status

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,10 +1,22 @@
 import Item from "./Item";
+import { useState } from "react";
 
 export default function ItemList({ items, onDeleteItem, onToggleItems }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems = [...items];
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description") {
+    sortedItems.sort((a, b) => a.description.localeCompare(b.description));
+  }
+  if (sortBy === "packed") {
+    sortedItems.sort((a, b) => Number(b.packed) - Number(a.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -13,6 +25,13 @@ export default function ItemList({ items, onDeleteItem, onToggleItems }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
